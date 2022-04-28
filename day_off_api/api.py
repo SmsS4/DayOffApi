8bbfebedaeb65cf9ebf64e924e7d17345cc950ab
SmsS4 <smss.lite@gmail.com>
2022-04-28 11:31:46 +0430

    Add methods

---
+++ day_off_api/api.py
@@ -0,0 +1,37 @@
+import requests
+import jdatetime
+from hijri_converter import Hijri, Gregorian
+
+HOST = "https://farsicalendar.com/api/"
+IS_THURSDAY_OFF = True
+
+
+def is_off_api(date_type: str, month: int, day: int) -> bool:
+    response = requests.get(f"{HOST}/{date_type}/{day}/{month}").json()
+    if len(response["values"]):
+        for value in response["values"]:
+            if value["dayoff"]:
+                return True
+    return False
+
+
+def is_day_off(month: int, day: int) -> bool:
+    shamsi = jdatetime.datetime(year=jdatetime.datetime.now().year, month=month, day=day)
+    if IS_THURSDAY_OFF and shamsi.weekday() == 5:
+        return True
+    if shamsi.weekday() == 6:
+        return True
+    # sh is for Shamsi
+    if is_off_api("sh", month, day):
+        return True
+    gregorian = shamsi.togregorian()
+    ghamari = Gregorian(gregorian.year, gregorian.month, gregorian.day).to_hijri()
+    # ic is for Ghamari
+    if is_off_api("ic", ghamari.month, ghamari.day):
+        return True
+    return False
+
+
+def is_today_off() -> bool:
+    today = jdatetime.datetime.now()
+    return is_day_off(today.month, today.day)
